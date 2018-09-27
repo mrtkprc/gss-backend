b628168d167382bf00972fdd9e8be33aa98a5636
mrtkprc <mrtkprc@gmail.com>
2018-09-27 13:03:14 +0300

    API Updated

--- models/SensorData.js
+++ models/SensorData.js
@@ -3,14 +3,8 @@ const Schema = mongoose.Schema;
 
 const SensorDataSchema = new Schema({
     sensor_location_id:Schema.Types.ObjectId,
-    sensor_date:{
-        type:Schema.Types.String,
-        default:Date.now
-    },
-    sensor_stimulations:{
-        type:Schema.Types.Array,
-        default:Date.now
-    },
+    sensor_date:Schema.Types.String,
+    sensor_stimulations:Schema.Types.Array,
     geriatric_id:Schema.Types.ObjectId
 });
 
--- routes/sensors.js
+++ routes/sensors.js
@@ -1,21 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
 const SensorData = require('../models/SensorData');
 const SensorLocation = require('../models/SensorLocation');
 
-
-
 router.post('/add/location/', (req, res, next) => {
-    /*
-    //Başka bir şey yapmak için böyle eklenmeli
-    const { name,surname,gsm,public_key } = req.body;
-    const value_added = new SensorLocation({
-        name,
-        surname,
-        gsm,
-        public_key
-    });
-    */
     //Sensor Location Added
     const value_added = new SensorLocation(req.body);
     const promise = value_added.save();
@@ -29,20 +18,35 @@ router.post('/add/location/', (req, res, next) => {
 
 router.post('/add/stimulus/', (req, res, next) => {
     //Sensor Data Added
-    const d = new Date.now();
-
+    const d = new Date();
+    const today = d.getFullYear().toString()+"-"+("0" + (d.getMonth() + 1)).slice(-2).toString()+"-"+("0" + d.getDate()).slice(-2).toString();
+    const today_with_hour = today+ " "+ ("0" + d.getHours()).slice(-2).toString()+":"+ ("0" + d.getMinutes()).slice(-2).toString()+":"+("0" + d.getSeconds()).slice(-2).toString();
     const {sensor_location_id,geriatric_id} = req.body;
-    const value_added = new SensorData({
-        sensor_location_id,
-        geriatric_id,
-        sensor_date:d.getFullYear().toString()+"-"+d.getMonth().toString()+"-"+d.getDay().toString()
-    });
-    const promise = value_added.save();
+    SensorData.countDocuments({ },( err, count) => {
+        if(count < 1)
+        {
+            const sensor_data = new SensorData({
+                sensor_location_id,
+                geriatric_id,
+                sensor_date:d.getFullYear().toString()+"-"+("0" + (d.getMonth() + 1)).slice(-2).toString()+"-"+("0" + d.getDate()).slice(-2).toString()
+            });
 
-    promise.then((data) => {
-        res.json(data);
-    }).catch((err) => {
-        res.json(err);
+            const value_added = sensor_data.save();
+            value_added.then((data) => {
+                res.json(data);
+            }).catch((err) => {
+                res.json(err);
+            });
+        }
+        else
+        {
+            const val = SensorData.updateOne({geriatric_id},{$push:{sensor_stimulations:today_with_hour}});
+            val.then((data) => {
+                res.json({result:"Updated."});
+            }).catch((err) => {
+                res.json(err);
+            });
+        }
     });
 });
 
