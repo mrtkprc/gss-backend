52c81a7831aa7fdd0454aedc30331d500237af39
mrtkprc <mrtkprc@gmail.com>
2018-10-28 20:01:43 +0300

    Telegram Bot Updated

--- models/Geriatric.js
+++ models/Geriatric.js
@@ -5,6 +5,7 @@ const GeriatricSchema = new Schema({
     name:Schema.Types.String,
     surname:Schema.Types.String,
     gsm:Schema.Types.String,
+    telegram_chat_ids:Schema.Types.Array,
     public_key: {
         type:Schema.Types.String,
         required:true,
